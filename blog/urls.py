d7338515566aa4e4417c4d0b62f761ccf17554ae
parham12 <plavigne19@gmail.com>
2017-07-30 06:59:31 -0700

    Post page support.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from  django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^post$', views.post, name='post'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^$', views.home, name='home'),
 ]
